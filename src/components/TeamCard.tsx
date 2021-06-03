5cba3ef73670586beb51851d29fd20f5b2fe5660
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-06-03 14:51:29 +0200

    Main element with two components finished

---
+++ src/components/TeamCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './TeamCard.css';
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+interface Props {
+  teammembers: {
+    role: string;
+    name: string;
+    skills: string | number;
+  };
+}
+function TeamCard({ teammembers }: Props) {
+  const Fade = require("react-reveal/Fade");
+  return (
+    <Fade duration={800} bottom>
+      <div className="expcard-main">
+        <div className="inner">
+          <Card elevation={1}>
+            <CardHeader title={teammembers.name} />
+            <CardContent>
+              <Typography variant="h5">{teammembers.role}</Typography>
+              <Typography variant="body2">{teammembers.skills}</Typography>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </Fade>
+  );
+}
+
+export default TeamCard;
---
+++ src/data/data.tsx
@@ -0,0 +1,30 @@
+
+export const greeting = {
+
+  note: "Hello World",
+  view: true,
+}
+
+
+
+export const teammembers = {
+  title: "Meet The Team",
+  lists: [
+    {
+      name: "Shahin",
+      role: "Test",
+      skills: "Front-End Developer",
+    },
+    {
+      name: "Linn",
+      role: "Linn",
+      skills: "Linn",
+    },
+    {
+      name: "Anton",
+      role: "Anton",
+      skills: "Anton",
+    },
+  ],
+  view: true,
+};
